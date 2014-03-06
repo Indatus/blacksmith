bae3bca6c7ab1b6342fa1798b8bf42545d6f7f01
Brian Webb <bwebb@indatus.com>
2014-03-06 06:41:43 -0500

    added functional test mock valu generator and test for it

--- src/lib/Generators/FunctionalTest.php
+++ src/lib/Generators/FunctionalTest.php
@@ -17,4 +17,69 @@ class FunctionalTest extends Generator implements GeneratorInterface
     {
         parent::__construct($filesystem, $mustache, $fieldParser);
     }
+
+    /**
+     * Function to get the minimum template variables
+     * 
+     * @return array
+     */
+    public function getTemplateVars()
+    {
+        $entity    = $this->getEntityName();
+        $fieldData = $this->getFieldData();
+
+        $attributes = [];
+
+        //get fake data for each property
+        foreach ($fieldData as $property => $meta) {
+            $attributes[$property] = $this->getAttributeMockValue($meta['type']);
+        }
+
+        //add override for specific cases like id, created_at etc
+        $attributes['id'] = 1;
+        $attributes['created_at'] = "'". date('Y-m-d H:i:s') ."'";
+        $attributes['updated_at'] = "'". date('Y-m-d H:i:s') ."'";
+
+        $mock_attribute_rows = [];
+
+        //create a "row" for each entry
+        foreach ($attributes as $key => $value) {
+            $mock_attribute_rows[] = "'{$key}' => {$value},";
+        }
+
+        return [
+            'Entity'          => Str::studly($entity),
+            'Entities'        => Str::plural(Str::studly($entity)),
+            'collection'      => Str::plural(Str::snake($entity)),
+            'instance'        => Str::singular(Str::snake($entity)),
+            'fields'          => $fieldData,
+            'mock_attributes' => $mock_attribute_rows
+        ];
+    }
+
+
+    /**
+     * Function to return the form element type
+     * that should be used, given the datatype input
+     * 
+     * @param  string $dataType Field data type
+     * @return string           Form element type to use
+     */
+    public function getAttributeMockValue($dataType)
+    {
+        $lookup = [
+            'integer'    => 1,
+            'bigInteger' => PHP_INT_MAX,
+            'string'     => "'dreamcatcher'",
+            'decimal'    => 12345.12,
+            'float'      => 152853.5047,
+            'timestamp'  => time(),
+            'date'       => "'". date('Y-m-d') ."'",
+            'dateTime'   => "'". date('Y-m-d H:i:s') ."'",
+            'text'       => "'Tonx art party PBR&B, Blue Bottle sriracha Bushwick iPhone wolf kale chips Godard typewriter selfies shabby chic church-key 3 wolf moon'",
+            'boolean'    => true
+        ];
+
+        return array_key_exists($dataType, $lookup) ? $lookup[$dataType] : "text";
+    }
 }
--- tests/Generators/FunctionalTestTest.php
+++ tests/Generators/FunctionalTestTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Filesystem\Filesystem;
 use Parsers\FieldParser;
 use Mustache_Engine;
+use Mockery as m;
 
 class FunctionalTestTest extends \BlacksmithTest
 {
@@ -15,5 +16,102 @@ class FunctionalTestTest extends \BlacksmithTest
             new FieldParser
         );
         $this->assertInstanceOf("Generators\Generator", $instance);
+    }
+
+    public function testGetAttributeMockValue()
+    {
+        $g = m::mock('Generators\FunctionalTest');
+        $g->shouldDeferMissing();
+
+        //check integers
+        $this->assertTrue(
+            is_int($g->getAttributeMockValue('integer')),
+            "Expected integer to return integer val"
+        );
+
+        //check big integers
+        $this->assertTrue(
+            is_int($g->getAttributeMockValue('bigInteger')),
+            "Expected bigInteger to return integer val"
+        );
+        $this->assertTrue(
+            ($g->getAttributeMockValue('bigInteger') <= PHP_INT_MAX),
+            "Expected bigInteger to be less than or equal to PHP_INT_MAX"
+        );
+        
+
+        //check strings
+        $stringVal = $g->getAttributeMockValue('string');
+        $this->assertTrue(
+            is_string($stringVal),
+            "Expected string to be a string"
+        );
+        $this->assertTrue(
+            ($stringVal[0] == "'" && $stringVal[strlen($stringVal)-1] == "'"),
+            "Expected string value to be sigle quoted"
+        );
+
+        //check decimals
+        $this->assertTrue(
+            is_double($g->getAttributeMockValue('decimal')),
+            "Expected decimal value to be a double"
+        );
+
+        //check floats
+        $this->assertTrue(
+            is_float($g->getAttributeMockValue('float')),
+            "Expected float value to be a float"
+        );
+        
+        //check timestamps
+        $this->assertTrue(
+            is_int($g->getAttributeMockValue('timestamp')),
+            "Expected timestamp to be an int"
+        );
+
+        //check dates
+        $date_regex = '/(19|20)\d\d[- -.](0[1-9]|1[012])[- -.](0[1-9]|[12][0-9]|3[01])/';
+        $dateVal    = $g->getAttributeMockValue('date');
+        $this->assertTrue(
+            preg_match($date_regex, str_replace("'", "", $dateVal)) > 0,
+            "Expected date [{$dateVal}] to be in YYYY-MM-DD format"
+        );
+        $this->assertTrue(
+            ($dateVal[0] == "'" && $dateVal[strlen($dateVal)-1] == "'"),
+            "Expected date [{$dateVal}] value to be sigle quoted"
+        );
+
+        //test datetimes
+        $datetime_regex = '/(19|20)\d\d[- -.](0[1-9]|1[012])[- -.](0[1-9]|[12][0-9]|3[01])\s(0[0-9]|1[0-9]|2[0123]):([0-5][0-9]):([0-5][0-9])/';
+        $dateTimeVal    = $g->getAttributeMockValue('dateTime');
+        $this->assertTrue(
+            preg_match($datetime_regex, str_replace("'", "", $dateTimeVal)) > 0,
+            "Expected dateTime [{$dateTimeVal}] to be in YYYY-MM-DD HH:ii:ss format"
+        );
+        $this->assertTrue(
+            ($dateTimeVal[0] == "'" && $dateTimeVal[strlen($dateTimeVal)-1] == "'"),
+            "Expected dateTime [{$dateTimeVal}] value to be sigle quoted"
+        );
+
+        //test text values
+        $textVal = $g->getAttributeMockValue('text');
+        $this->assertTrue(
+            is_string($textVal),
+            "Expected text value to be a string"
+        );
+        $this->assertTrue(
+            ($textVal[0] == "'" && $textVal[strlen($textVal)-1] == "'"),
+            "Expected text value to be sigle quoted"
+        );
+        $this->assertTrue(
+            strlen($textVal) > 25,
+            "Expected text value to be greater than 25 characters"
+        );
+
+        //test booleans
+        $this->assertTrue(
+            is_bool($g->getAttributeMockValue('boolean')),
+            "Expected boolean value to be bool"
+        );
     }
 }
